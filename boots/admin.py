b98c13738ee6ab7a1a520b226eb9310e9d78cb83
Declan446 <Declan.446@outlook.com>
2020-08-28 14:56:18 +0000

    added boots app models and done migrations

--- boots/admin.py
+++ boots/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Boot, Brand
 
 # Register your models here.
+admin.site.register(Boot)
+admin.site.register(Brand)
--- boots/models.py
+++ boots/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+class Brand(models.Model):
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Boot(models.Model):
+    category = models.ForeignKey('Brand', null=True, blank=True, on_delete=models.SET_NULL)
+    style = models.CharField(max_length=254)
+    name = models.CharField(max_length=254)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    colour = models.CharField(max_length=254)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
