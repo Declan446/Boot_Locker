d18d63e3e4c8cf725dd8d50db07f12d2937725e1
Declan446 <Declan.446@outlook.com>
2020-08-28 16:48:37 +0000

    added boots app views and templates

--- boots/views.py
+++ boots/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Boot
 
 # Create your views here.
+
+
+def all_boots(request):
+    """ A view to show all boots, including sorting and search queries """
+
+    boots = Boot.objects.all()
+
+    context = {
+        'boots': boots,
+    }
+
+    return render(request, 'boots/boots.html', context)
