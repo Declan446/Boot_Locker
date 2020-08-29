9447afe4f6bc7de09180e039523e15e9626e6fc7
Declan446 <Declan.446@outlook.com>
2020-08-29 19:09:36 +0000

    added boot details functionality

--- boots/urls.py
+++ boots/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.all_boots, name='boots')
+    path('', views.all_boots, name='boots'),
+    path('<boot_id>', views.boot_detail, name='boot_detail'),
 ]
--- boots/views.py
+++ boots/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Boot
 
 # Create your views here.
@@ -14,3 +14,15 @@ def all_boots(request):
     }
 
     return render(request, 'boots/boots.html', context)
+
+
+def boot_detail(request, boot_id):
+    """ A view to show selected boot detail """
+
+    boot = get_object_or_404(Boot, pk=boot_id)
+
+    context = {
+        'boot': boot,
+    }
+
+    return render(request, 'boots/boot_detail.html', context)
