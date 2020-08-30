dd11c3e63b3707544fc988deb24f84f27bc02d28
Declan446 <Declan.446@outlook.com>
2020-08-30 15:54:13 +0000

    added filtering for the nav bar by style for the dropdowns Adidas, Nike and New Balance. the filtering by ctegory still needs work. the boots can also be filtered by price low to high/high to low

--- boots/views.py
+++ boots/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Boot
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
+from .models import Boot, Brand
 
 # Create your views here.
 
@@ -8,9 +10,51 @@ def all_boots(request):
     """ A view to show all boots, including sorting and search queries """
 
     boots = Boot.objects.all()
+    query = None
+    brands = None
+    sort = None
+    direction = None
+    style = None
+
+    if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                boots = boots.annotate(lower_name=Lower('name'))
+
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            boots = boots.order_by(sortkey)
+
+        if 'style' in request.GET:
+            boots = request.GET['style'].split(',')
+            boots = Boot.objects.filter(style__in=boots)
+
+        if 'brand' in request.GET:
+            brands = request.GET['brand'].split(',')
+            boots = boots.filter(category__name__in=brands)
+            brands = Brand.objects.filter(name__in=brands)
+
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter a valid search item!")
+                return redirect(reverse('boots'))
+
+            queries = Q(name__icontains=query) | Q(colour__icontains=query)
+            boots = boots.filter(queries)
+
+    current_sorting = f'{sort}_{direction}'
 
     context = {
         'boots': boots,
+        'search_term': query,
+        'current_brands': brands,
+        'current_sorting': current_sorting,
     }
 
     return render(request, 'boots/boots.html', context)
