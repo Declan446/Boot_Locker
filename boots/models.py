8009b80ea8c859d897431df262163ae9433edb1f
Declan446 <Declan.446@outlook.com>
2020-09-01 12:01:16 +0000

    added sizes to boot model and size select box to boot template

--- boots/models.py
+++ boots/models.py
@@ -17,6 +17,7 @@ class Boot(models.Model):
     style = models.CharField(max_length=254)
     name = models.CharField(max_length=254)
     price = models.DecimalField(max_digits=6, decimal_places=2)
+    has_sizes = models.BooleanField(default=False, null=True, blank=True)
     colour = models.CharField(max_length=254)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
