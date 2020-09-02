2a95c4203c584b98276332db7346036f7d46a0b1
Declan446 <Declan.446@outlook.com>
2020-09-02 17:48:37 +0000

    added checkout views and templates

--- boot_locker/settings.py
+++ boot_locker/settings.py
@@ -45,6 +45,9 @@ INSTALLED_APPS = [
     'boots',
     'shopping_bag',
     'checkout',
+
+    # Other (allows us to format our forms with bootstrap)
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -58,6 +61,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'boot_locker.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -73,8 +78,13 @@ TEMPLATES = [
                 'django.template.context_processors.request',  # required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
                 'shopping_bag.contexts.bag_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
